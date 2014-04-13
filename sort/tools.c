9f236721ea4f4d9657778e677a253105e664d51c
suddenlydream <suddenlydream@163.com>
2014-04-14 00:35:23 +0800

    add tools

---
+++ sort/tools.c
@@ -0,0 +1,9 @@
+//
+//  tools.c
+//  sort
+//
+//  Created by dreaner on 14-4-14.
+//  Copyright (c) 2014年 dreaner. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ sort/tools.h
@@ -0,0 +1,14 @@
+//
+//  tools.h
+//  sort
+//
+//  Created by dreaner on 14-4-14.
+//  Copyright (c) 2014年 dreaner. All rights reserved.
+//
+
+#ifndef sort_tools_h
+#define sort_tools_h
+
+
+
+#endif
